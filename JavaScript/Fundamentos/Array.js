f10e410040416b417a76f674d634b585b311926a
Jorge Nascimento <jorg3nascimento@gmail.com>
2021-01-20 11:26:24 -0300

    Finalizando fundamentos de JS

---
+++ JavaScript/Fundamentos/Array.js
@@ -0,0 +1,247 @@
+console.log(typeof Array, typeof new Array, typeof [])
+
+let aprovados = new Array('Bia', 'Carlos', 'Ana')
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
+
+aprovados[3] = 'Paulo'
+aprovados.push('Abia')
+console.log(aprovados.length)
+
+aprovados[9] = 'Rafael'
+console.log(aprovados.length)
+console.log(aprovados[8] === undefined)
+
+console.log(aprovados)
+aprovados.sort()
+console.log(aprovados)
+
+delete aprovados[1]
+console.log(aprovados[1])
+console.log(aprovados[2])
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+aprovados.splice(1, 1)
+console.log(aprovados)
+
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // massa quebrou o carro!
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro!
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')
+console.log(pilotos)
+
+// splice pode adicionar e remover elementos
+
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1) // massa quebrou :(
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2) // novo array
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
+
+// simulando array
+
+const quaseArray = { 0: 'Rafael', 1: 'Ana', 2: 'Bia' }
+console.log(quaseArray)
+Object.defineProperty(quaseArray, 'toString', {
+    value: function() { return Object.values(this) },
+    enumerable: false
+})
+
+console.log(quaseArray[0])
+
+const meuArray = ['Rafael', 'Ana', 'Bia']
+console.log(quaseArray.toString(), meuArray)
+
+// Implementando forEach
+
+Array.prototype.forEach2 = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this)
+  }
+}
+
+aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+  aprovados.forEach2(function(nome, indice) {
+  console.log(`${indice + 1}) ${nome}`)
+})
+
+// MAP
+
+const nums = [1, 2, 3, 4, 5]
+
+// For com propósito
+let resultado = nums.map(function(e) {
+    return e * 2
+})
+
+console.log(resultado)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+console.log(resultado)
+
+const carrinho = [
+  '{ "nome": "Borracha", "preco": 3.45 }',
+  '{ "nome": "Caderno", "preco": 13.90 }',
+  '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+  '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+// Retornar um array apenas com os preços
+
+const resultCart = carrinho.map((e) => JSON.parse(e)).map((e) => e.preco)
+
+console.log(resultCart) // carrinho[0].preco ...
+
+//
+
+Array.prototype.map2 = function(callback) {
+  const newArray = []
+  for (let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i], i, this))
+  }
+  return newArray
+}
+
+const carrinho = [
+  '{ "nome": "Borracha", "preco": 3.45 }',
+  '{ "nome": "Caderno", "preco": 13.90 }',
+  '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+  '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+// Retornar um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log(resultado)
+
+// Filter
+
+const produtos = [
+  { nome: 'Notebook', preco: 2499, fragil: true },
+  { nome: 'iPad Pro', preco: 4199, fragil: true },
+  { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+  { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+console.log(produtos.filter(function(p) {
+  return false
+}))
+
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+
+console.log(produtos.filter(caro).filter(fragil))
+
+// reduce
+
+const alunos = [
+  { nome: 'João', nota: 7.3, bolsista: false },
+  { nome: 'Maria', nota: 9.2, bolsista: true },
+  { nome: 'Pedro', nota: 9.8, bolsista: false },
+  { nome: 'Ana', nota: 8.7, bolsista: true }
+]
+
+console.log(alunos.map(a => a.nota))
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual) {
+  console.log(acumulador, atual)
+  return acumulador + atual
+}, 0)
+
+console.log(resultado)
+
+//
+
+const alunos = [
+  { nome: 'João', nota: 7.3, bolsista: false },
+  { nome: 'Maria', nota: 9.2, bolsista: true },
+  { nome: 'Pedro', nota: 9.8, bolsista: false },
+  { nome: 'Ana', nota: 8.7, bolsista: true }
+]
+
+// Desafio 1: Todos os alunos são bolsista?
+const todosBolsistas = (resultado, bolsista) => resultado && bolsista
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
+
+// Desafio 2: Algum aluno é bolsista?
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
+
+//
+
+Array.prototype.reduce2 = function(callback, valorInicial) {
+  const indiceInicial = valorInicial ? 0 : 1
+  let acumulador = valorInicial || this[0]
+  for (let i = indiceInicial; i < this.length; i++) {
+      acumulador = callback(acumulador, this[i], i, this)
+  }
+  return acumulador
+}
+
+const soma = (total, valor) => total + valor
+const nums = [1, 2, 3, 4, 5, 6]
+console.log(nums.reduce2(soma, 21))
+
+// flatMap
+
+const escola = [{
+  nome: 'Turma M1',
+  alunos: [{
+      nome: 'Gustavo',
+      nota: 8.1
+  }, {
+      nome: 'Ana',
+      nota: 9.3
+  }]
+}, {
+  nome: 'Turma M2',
+  alunos: [{
+      nome: 'Rebeca',
+      nota: 8.9
+  }, {
+      nome: 'Roberto',
+      nota: 7.3
+  }]
+}]
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1)
+
+console.log([].concat([ 8.1, 9.3 ], [ 8.9, 7.3 ]))
+
+Array.prototype.flatMap = function(callback) {
+  return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap(getNotasDaTurma)
+console.log(notas2)
---
+++ JavaScript/Fundamentos/Object.js
@@ -0,0 +1,197 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+ // OO
+ objeto = {
+     valor1,
+     valor2,
+     valor3,
+     processamento() {
+         // ...
+     }
+ }
+
+ objeto.processamento() // Foco passou a ser o objeto
+
+ // Principios importantes:
+ // 1. abstracao
+ // 2. encapsulamento
+ // 3. herança (prototype)
+ // 4. polimorfismo
+
+ // coleção dinâmica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 42
+    }],
+    calcularValorSeguro: function() {
+        // ...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
+console.log(carro)
+
+// delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+console.log(carro)
+console.log(carro.condutores)
+console.log(carro.condutores.length)
+
+// Criando Objetos
+
+// usando a notação literal
+const obj1 = {}
+console.log(obj1)
+
+// Object em JS
+console.log(typeof Object, typeof new Object)
+const obj2 = new Object
+console.log(obj2)
+
+// Funções construtoras
+function Produto(nome, preco, desc) {
+    this.nome = nome
+    this.getPrecoComDesconto = () => {
+        return preco * (1 - desc)
+    }
+}
+
+const p1 = new Produto('Caneta', 7.99, 0.15)
+const p2 = new Produto('Notebook', 2998.99, 0.25)
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+// Função Factory
+function criarFuncionario(nome, salarioBase, faltas) {
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario() {
+            return (salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+
+const f1 = criarFuncionario('João', 7980, 4)
+const f2 = criarFuncionario('Maria', 11400, 1)
+console.log(f1.getSalario(), f2.getSalario())
+
+// Object.create
+const filha = Object.create(null)
+filha.nome = 'Ana'
+console.log(filha)
+
+// Um função famosa que retorna Objeto...
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}')
+console.log(fromJSON.info)
+
+// Const endereça a referencia do objeto
+
+// pessoa -> 123 -> {...}
+const pessoa = { nome: 'Joao' }
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+// pessoa -> 456 -> {...}
+// pessoa = { nome: 'Ana' }
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({ nome: 'Joao' })
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
+
+
+// Get e set
+
+const sequencia = {
+  _valor: 1, // convenção
+  get valor() { return this._valor++ },
+  set valor(valor) { 
+      if(valor > this._valor) {
+          this._valor = valor
+      }
+  }
+}
+
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 1000
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 900
+console.log(sequencia.valor, sequencia.valor)
+
+
+// Funções importantes
+
+
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`)
+})
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true,
+    writable: false,
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
+
+// Object.assign (ECMAScript 2015)
+const dest = { a: 1 }
+const o1 = { b: 2 }
+const o2 = { c: 3, a: 4 }
+const obj = Object.assign(dest, o1, o2)
+
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
+
+    

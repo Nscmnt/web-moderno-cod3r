d707dd1a9ecfe6f5a20905a351c0e40d267cba81
Jorge Nascimento <jorg3nascimento@gmail.com>
2021-03-01 13:59:17 -0300

    Fianlizando o modulo de HTML

--- HTML/main.js
+++ HTML/main.js
@@ -1,15 +1,13 @@
 document.querySelectorAll('a').forEach((link) => {
   const conteudo = document.getElementById('conteudo')
 
-  link.onclick = function(e){
+  link.addEventListener('click', function(e){
     e.preventDefault()
 
     fetch(link.href)
     .then( res => res.text())
-    .then( html => conteudo.innerHTML = html)
+    .then( html => conteudo.innerHTML = html) 
 
-  }
+    })
 })
 
-const lista = document.querySelector('.tree')
-console.log("olá")
